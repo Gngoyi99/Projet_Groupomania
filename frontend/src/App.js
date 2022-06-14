4fb28090a8e5cd4e5aaa9a53c38c5be769666a70
Gngoyi99 <73467477+Gngoyi99@users.noreply.github.com>
2022-06-14 10:54:00 +0200

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import Routes from "./components/Routes";
+
+const App = () => {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/Acceuil.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Acceuil = () => {
+  return (
+    <div>
+      <div>BIENVENUE</div>
+    </div>
+  );
+};
+
+export default Acceuil;

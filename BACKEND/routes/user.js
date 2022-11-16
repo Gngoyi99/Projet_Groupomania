cf1664023628004f7d9c2811959dc03181bb5737
Gngoyi99 <73467477+Gngoyi99@users.noreply.github.com>
2022-11-16 10:25:23 +0100

    Delete BACKEND directory

--- BACKEND/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userCtrl = require("../controllers/user");
-const password = require("../middleware/password");
-
-router.post("/signup", password, userCtrl.signup);
-router.post("/login", userCtrl.login);
-
-module.exports = router;
--- BACKEND/server.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config({ path: "./config/.env" });
-const app = require("./app");
-
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
